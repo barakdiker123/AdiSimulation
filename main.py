54ea6f5a89f93b69c18fcabf1e5469d7e135e51c
barak diker <barakdiker@gmail.com>
2023-05-18 14:28:30 +0300

    This is a test

--- main.py
+++ main.py
@@ -2,3 +2,27 @@
 
 
 print("Hello")
+
+
+
+class DF:
+    name="non"
+    score
+    def generic_cognitive(self):
+        print("This is a generic cognitive message")
+
+    def generaric_heuristic(self):
+        print("This is a generic cognitive message")
+
+
+
+#input(DF )
+
+
+##def __main__():
+##
+#import Tkinter
+#top = Tkinter.Tk()
+## Code to add widgets will go here...
+#top.mainloop()
+#print("barak")
